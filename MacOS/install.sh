4adea02361fc2d5b5e9492b8e0017daabb1a3654
Martantoine <martantoine@tuta.io>
2022-08-04 16:51:41 +0200

    while changes

--- MacOS/install.sh
+++ MacOS/install.sh
@@ -65,18 +65,14 @@ brew install git
 brew tap microsoft/git
 brew install --cask git-credential-manager-core
 
-echo
-echo "Make sure there are no spaces in your installation path!"
-read -p "InstallPath by default is ~/Applications" InstallPath
-InstallPath=${InstallPath:-~/Applications/}
-
-echo
-echo "Are you sure you want it to be installed at $InstallPath ?"
-read -p "Enter y for Yes, any word for No: " ans
+ans=n
 while [ $ans != y ]; do
     echo
     read -p "InstallPath by default is ~/Applications" InstallPath
-    InstallPath=${InstallPath:-~/Applications/}
+    InstallPath=${InstallPath:-~/Applications}
+    echo
+    echo "Are you sure you want it to be installed at $InstallPath ?"
+    read -p "Enter y for Yes, any word for No: " ans
 done
 
 echo
@@ -148,7 +144,7 @@ SettingsPath=${InstallPath//\//\/\/}
 echo "{" >> settings.json
 echo "	\"explorer.confirmDelete\": false," >> settings.json
 echo "	\"gcc_arm_path\": \"$SettingsPath/$programVS//Contents//gcc-arm-none-eabi-7-2017-q4-major\"," >> settings.json
-echo "	\"epuck_tools\": \"$SettingsPath/$programVS//Contents//Tools\"," >> settings.json
+echo "	\"epuck_tools\": \"$InstallPath/$programVS/Contents/Tools\"," >> settings.json
 echo "	\"workplace\": \"$Workplace\"" >> settings.json
 echo "}" >> settings.json
 

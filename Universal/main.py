431cd58f9c6da88a903c4e68fbb68c6517fd44e1
Antoine Vincent Martin <martantoine@tuta.io>
2024-01-29 10:08:14 +0100

    stipped app from kivy step 1

--- Universal/main.py
+++ Universal/main.py
@@ -1,7 +1,7 @@
 from kivy.app import App
 from kivy.uix.settings import SettingsWithTabbedPanel
 from kivy.uix.label import Label
-from kivy.logger import Logger
+#from kivy.logger import Logger
 from kivy.lang import Builder
 from kivy.config import Config
 from kivy.clock import Clock
@@ -20,50 +20,24 @@ BoxLayout:
     orientation: 'vertical'
     id: pages
     BoxLayout:
-        orientation: 'horizontal'
-        BoxLayout:
-            id: page1
-            canvas:
-                Color:
-                    rgb: 0, 0, 0, 1
-                Rectangle:
-                    pos: self.pos
-                    size: self.size
-            orientation: 'vertical'
-            Button:
-                text: 'Install VSCode EPuck2'
-                on_release: app.install()
-            Button:
-                text: 'Uninstall VSCode EPuck2'
-                on_release: app.uninstall()
-            Button:
-                text: 'Settings (or press F1)'
-                on_release: app.open_settings()
-        BoxLayout:
-            id: page2
-            disabled: True
-            canvas:
-                Color:
-                    rgb: 0, 0, 0, 1
-                Rectangle:
-                    pos: self.pos
-                    size: self.size
-            orientation: 'vertical'
-            ScrollView:
-                Label:
-                    id: console
-                    size_hint: 1, None
-                    size: self.texture_size
-            ProgressBar:
-                id: progressbar
-                size_hint: 1, 0.1
-                opacity: 0
-                max: 1000
-                value: 0
-    Button
-        size_hint: 1, 0.2
-        text: 'Exit'
-        on_release: app.stop()
+        id: page1
+        canvas:
+            Color:
+                rgb: 0, 0, 0, 1
+            Rectangle:
+                pos: self.pos
+                size: self.size
+        orientation: 'vertical'
+        Button:
+            text: 'Install VSCode EPuck2'
+            on_release: app.install()
+        Button:
+            text: 'Settings (or press F1)'
+            on_release: app.open_settings()
+        Button
+            size_hint: 1, 0.2
+            text: 'Cancel'
+            on_release: app.cancel()
 '''
 
 settings_json = '''
@@ -148,13 +122,14 @@ settings_json = '''
 ]
 '''
 
+install = False
+
 class MyApp(App):
     use_kivy_settings = False # otherwise polluted by useless GUI settings
 
     def build(self):
-        Logger.info("Installer starting")
+        #Logger.info("GUI starting")
         root = Builder.load_string(kv)
-        root.ids.console.text = console
         return root
 
     def build_config(self, config):
@@ -164,55 +139,42 @@ class MyApp(App):
         settings.add_json_panel('VSCode EPuck2 settings', self.config, data=settings_json)
 
     def on_config_change(self, config, section, key, value):
-        Logger.info("main.py: App.on_config_change: {0}, {1}, {2}, {3}".format(
-            config, section, key, value))
+        #Logger.info("main.py: App.on_config_change: {0}, {1}, {2}, {3}".format(
+        #    config, section, key, value))
         installer.settings.dict[key] = value
 
     def close_settings(self, settings=None):
-        Logger.info("main.py: App.close_settings: {0}".format(settings))
+        #Logger.info("main.py: App.close_settings: {0}".format(settings))
         super(MyApp, self).close_settings(settings)
 
-    def console_push_back(self, text):
-        global console
-        console = console + text
-        self.root.ids.console.text = console
+    def cancel(self):
+        global install
+        install = False
+        self.stop()
 
     def install(self):
-        Clock.schedule_interval(self.wip, 0.5)
-        self.root.ids.progressbar.value = 500
-        self.root.ids.page1.disabled = True
-        self.root.ids.page2.disabled = False
-        self.root.ids.progressbar.opacity = 1
         for key in installer.settings.dict:
             installer.settings.dict[key] = self.config.get('settings', key)
         
-        Logger.info("Installation is starting!!")
-        installer.init_folders()
-        installer.step1()
-        time.sleep(1)
-        installer.step2()
-        time.sleep(1)
-        installer.step3()
-        time.sleep(1)
-        installer.step4()
-        time.sleep(1)
-        installer.step5()
-        time.sleep(1)
-        installer.step6()
-        logging.info("Installation done!!")
-        
-    def uninstall(self):
-        Logger.info("Uninstall")
-        self.root.ids.page1.disabled = True
-        self.root.ids.page2.disabled = False
-        self.root.ids.progressbar.opacity = 1
-
-    def wip(self, *largs):
-        self.root.ids.progressbar.value = random.random() * 1000
-
-
-h = logging.FileHandler("install" + time.strftime("%Y-%m-%d-%H-%M-%S") + ".log")
-h.setLevel(logging.INFO)
-Logger.addHandler(h)
+        global install
+        install = True
+        self.stop()
 
 MyApp().run()
+log_file = "install.log" + time.strftime("%Y-%m-%d-%H-%M-%S") + ".log"
+logging.basicConfig(filename=log_file, encoding='utf-8', level=logging.INFO)
+logging.error("Log saved at: " + log_file)
+
+if install:
+    logging.debug("Installation starting")
+    logging.info("Settings: " + str(installer.settings.dict))
+    installer.init_folders()
+    installer.step1()
+    installer.step2()
+    installer.step3()
+    installer.step4()
+    installer.step5()
+    installer.step6()
+    logging.debug("Installation complete")
+else:
+    logging.debug("Installation cancelled")
